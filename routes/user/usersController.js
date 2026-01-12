0e43802ac96dca6412e269094d9b4926eb53401e
glf30 <greyson.frazier@gmail.com>
2026-01-12 12:11:26 -0500

    Add encryption for when user is created

--- routes/user/usersController.js
+++ routes/user/usersController.js
@@ -1,9 +1,29 @@
 const User = require("./usersModel");
+const bcrypt = require("bcrypt");
 
 const createUser = async (userData) => {
     try {
-        const newUser = await User.create(userData);
-        return newUser;
+    
+        // 1. generate the salt
+        const salt = await bcrypt.genSalt();
+
+        // 2. encrypt the password
+        // pass in the password from the request body, along with the salt
+        const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+        // 3. create secure user object
+        // store the username along with the encrypted password inside a new object
+        const secureUserData = {
+            username: userData.username,
+            password: hashedPassword
+        }
+
+        const newUser = await User.create(secureUserData);
+
+        // prevent password from being returned
+        const returnedUser = await User.findOne({ username: userData.username }).select("-password")
+
+        return returnedUser;
     } catch (error) {
         throw error;
     }
